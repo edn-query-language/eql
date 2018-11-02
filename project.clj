e30fa664243a6f257c09e5a86b8162a9006efa24
Wilker Lucio <wilkerlucio@gmail.com>
2018-11-02 16:52:08 -0300

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject edn-query-language/eql "0.0.2"
+(defproject edn-query-language/eql "0.0.3"
   :description "EDN Query Language support library"
   :url "https://github.com/edn-query-language/eql"
   :license {:name "MIT" :url "https://opensource.org/licenses/MIT"}
