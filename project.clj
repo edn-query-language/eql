bf9dd1d4891de3a1df506c32fff0c3db5f2ee606
Tony Kay <tony.kay@gmail.com>
2025-10-22 11:59:41 +0300

    added ref to clj command

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
 
   :plugins [[lein-tools-deps "0.4.1"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
-  :lein-tools-deps/config {:config-files [:install :user :project]}
+  :lein-tools-deps/config {:config-files [:install :user :project]
+                           :clojure-executables ["/opt/homebrew/bin/clojure"]}
 
   :jar-exclusions [#"public/.*" #"\.DS_Store"])
